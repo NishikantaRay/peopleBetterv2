dfef822a1fb6034cf62964a11d4a3d265c5dfe11
NishikantaRay <nishikantaray1@gmail.com>
2025-08-11 22:10:56 +0530

    added

---
+++ js/navbar-scroll.js
@@ -0,0 +1,9 @@
+// Navbar scroll effect: add 'scrolled' class when page is scrolled
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  if (window.scrollY > 10) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
